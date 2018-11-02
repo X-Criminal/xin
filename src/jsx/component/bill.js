cf8cc65a9de090ce745093ac1f20a4e49875b789
Xiao <763790998@qq.com>
2018-11-02 13:52:57 +0800

    2018/11/12

--- src/jsx/component/bill.js
+++ src/jsx/component/bill.js
@@ -1,18 +1,31 @@
-import React ,{Component} from "react"
-
+import React ,{Component} from "react";
+import {Input,Button,DatePicker}     from "antd"
+import locale from 'antd/lib/date-picker/locale/zh_CN';
 export default class App extends Component{
     constructor(props){
         super(props)
         this.state={
-
+            loading:false
         }
     }
-
+    search=( )=>{
+        
+    }
         render(){
             return(
                 <div className={"admin bill"}>
                        <h3>提现账单</h3>
-                       
+                       <div className={"adminSearch clear-fix"}>
+                            <span>搜索：</span>
+                            <Input className={"keyWord"}  />
+                            <span style={{margin:"0 8px"}}>起</span>
+                            <DatePicker locale={locale}/>
+                            <span style={{margin:"0 8px"}}>止</span>
+                            <DatePicker locale={locale}/>
+                            <Button className={"Adminbtn"} type="primary" loading={this.state.loading} onClick={this.search}>
+                                查询
+                            </Button>
+                       </div>
                 </div>
             )
         }
